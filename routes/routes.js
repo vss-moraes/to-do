f154ae7fbfb965ee9609e1441470a38d5efc7aa4
Vinicius Moraes <vsantana0212@gmail.com>
2019-12-21 18:55:37 -0400

    Limpando boilerplate code

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var tarefas = []
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Gerenciador de Tarefas' });
@@ -8,6 +10,11 @@ router.get('/', function(req, res, next) {
 
 router.get('/adicionar', function(req, res, next) {
   res.render('form', {title: 'Adicionar Tarefa'})
+})
+
+router.post('/adicionar', function(req, res, next) {
+  tarefas.push(req.body)
+  res.render('index', { title: 'Gerenciador de Tarefas' })
 })
 
 module.exports = router;
