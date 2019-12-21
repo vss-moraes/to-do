384cb01d8fc6e8f16a14bd61d8e77efe70435a5e
Vinicius Moraes <vsantana0212@gmail.com>
2019-12-21 16:54:00 -0400

    Criação da página de cadastro de tarefas

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Gerenciador de Tarefas' });
 });
+
+router.get('/adicionar', function(req, res, next) {
+  res.render('form', {title: 'Adicionar Tarefa'})
+})
 
 module.exports = router;
